9b174d119854a9536c56bb7a7da6ef3cb8a63cab
BATOOLMAHMOOD <batoolmahmood04@gmail.com>
2025-03-20 00:53:48 +0300

    making task7

--- task7/task7.js
+++ task7/task7.js
@@ -1,36 +1,69 @@
-const express = require("express");
- const app = express();
- 
- app.use(express.urlencoded({ extended: true }));
- app.use(express.json());
- 
- let scores = [];
- 
- 
- app.post("/scores", (req, res) => {
-     const score = {
-         id: scores.length + 1,
-         player: req.body.player,
-         score: req.body.score
-     };
-     scores.push(score);
-     res.json({ message: "Score Submitted", score });
- });
- 
- 
- app.get("/scores", (req, res) => {
-     const sortedScores = scores.sort((a, b) => b.score - a.score);
-     res.json(sortedScores);
- });
- 
- 
- app.delete("/scores/:id", (req, res) => {
-     const id = parseInt(req.params.id);
-     const initialLength = scores.length;
-     scores = scores.filter(s => s.id !== id);
-     res.json({ message: "Score Deleted" });
- });
- 
- app.listen(3000, () => {
-     console.log("Bowling Score API running on port 3000");
- });
+//CREATE TABLE scores (
+   // id SERIAL PRIMARY KEY,
+   // player_name VARCHAR(255) NOT NULL,
+   // score INT NOT NULL,
+   // created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+//);
+//هدول عشان اعمل انشاء جدول بال sql
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+module.exports = pool;
+const express = require('express');
+const pool = require('./db');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+
+app.post('/scores', async (req, res) => {
+    try {
+        const { player_name, score } = req.body;
+        const newScore = await pool.query(
+            "INSERT INTO scores (player_name, score) VALUES ($1, $2) RETURNING *",
+            [player_name, score]
+        );
+        res.json(newScore.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+
+app.get('/scores', async (req, res) => {
+    try {
+        const allScores = await pool.query(
+            "SELECT * FROM scores ORDER BY score DESC, created_at ASC"
+        );
+        res.json(allScores.rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+
+app.delete('/scores/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        await pool.query("DELETE FROM scores WHERE id = $1", [id]);
+        res.json({ message: "Score deleted successfully" });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+
+app.listen(PORT, () => {
+    console.log("Server is running on port $ {PORT}");
+});
